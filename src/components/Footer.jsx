9e7753257d062cb8a73fa8e1f5eb1b0858382649
Pedro Nest <pedronestjp@gmail.comgit config --global user.name Pedro>
2023-06-02 00:27:22 -0300

    config firebase

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Footer.module.css";
 import { IoPaperPlaneOutline } from "react-icons/io5";
+import { collection, addDoc } from "firebase/firestore";
+import {  getToken, onMessage } from "firebase/messaging";
+import { db, messaging } from "../lib/firebase";
+
 
 const Footer = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Salvar os dados no Firestore
+      await addDoc(collection(db, "messages"), {
+        name: name,
+        email: email,
+        message: message,
+        timestamp: new Date(),
+      });
+
+      // Obter o token de envio de mensagens push
+      const token = await getToken(messaging);
+
+      // Enviar mensagem push usando o serviço de mensagens push
+      await fetch("https://fcm.googleapis.com/fcm/send", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: {
+            notification: {
+              title: "Nova mensagem recebida",
+              body: `Você recebeu uma nova mensagem de ${name}`,
+            },
+            token: token,
+          },
+        }),
+      });
+
+      // Sucesso ao enviar mensagem push
+      console.log("Mensagem push enviada com sucesso");
+    } catch (error) {
+      // Erro ao enviar mensagem push
+      console.error("Erro ao enviar mensagem push:", error);
+    }
+
+    // Limpar os campos após o envio
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.contacts}>
-          <p className={styles.contatcText}> Contato </p>
-          <a href="https://mail.google.com/mail/u/0/#inbox"> <p className={styles.email}> meudigitalagencia@gmail.com </p> </a>
-          <p className={styles.phone}> 83 9 86377109 </p>
+          <p className={styles.contatcText}>Contato</p>
+          <a href="https://mail.google.com/mail/u/0/#inbox">
+            <p className={styles.email}>meudigitalagencia@gmail.com</p>
+          </a>
+          <p className={styles.phone}>83 9 86377109</p>
         </div>
-        <div className={styles.newslatter}>
-          <p className={styles.signUpNewsLatter}> Inscreva-se na nossa Newslatter </p>
-          <input type='submit' className={styles.submitEmail} 
-          placeholder="Coloque seu e-mail aqui" /> <IoPaperPlaneOutline/>
-        </div>
+        <form onSubmit={handleFormSubmit}>
+          <div className={styles.newslatter}>
+            <p className={styles.signUpNewsLatter}>
+              Inscreva-se na nossa Newsletter
+            </p>
+            <input
+              type="text"
+              className={styles.submitEmail}
+              placeholder="Coloque seu e-mail aqui"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type="submit" hidden></button>
+            <IoPaperPlaneOutline />
+          </div>
+          <div className={styles.contactForm}>
+            <input
+              type="text"
+              className={styles.name}
+              placeholder="Seu nome"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <textarea
+              className={styles.message}
+              placeholder="Mensagem"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+            <button type="submit" className={styles.submitButton}>
+              Enviar Mensagem
+            </button>
+          </div>
+        </form>
         <div>
-        <img
-            src="./images/logoNew1.png"
+          <img
+            src="./images/logoMeuWhite.png"
             alt="Logo"
             className={styles.logo}
           />
-          <p className={styles.textBtnLogo}> Vamos evoluir juntos? </p>
+          <p className={styles.textBtnLogo}>Vamos evoluir juntos?</p>
         </div>
       </div>
     </footer>
